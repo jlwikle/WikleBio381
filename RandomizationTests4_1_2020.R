4c507a378d252d2c6006c47d147f581875c29e81
jlwikle <jessica.wikle@uvm.edu>
2020-04-13 15:50:38 -0400

    upload homework 11

--- RandomizationTests4_1_2020.R
+++ RandomizationTests4_1_2020.R
@@ -89,26 +89,129 @@ print(df_sim)
 obs_sim <- tapply(df_sim$res, df_sim$trt, mean)
 print(obs_sim)
 
+# build functions ------------------------------
 
+# -------------------------------
+# FUNCTION read_data
+# description: read in (or generate) data for analysis
+# inputs: file name (or nothing, as in this demo)
+# outputs: 3 column data frame of observed data (ID, x, y)
+###################################
+read_data <- function(z = NULL){
+  if(is.null(z)){
+    x_obs <- 1:20
+    y_obs <- x_obs + 10 * rnorm(20)
+    df <- data.frame(ID = seq_along(x_obs),
+                     x_obs,
+                     y_obs)
+  }
+#df <- read.table(file = z,
+ #                header = TRUE,
+ #                stringsAsFactors = FALSE)
+return(df)
 
+} # end of read_data
+#--------------------------------
+#read_data()
+# -------------------------------
+# FUNCTION get_metric
+# description: calculate metric for randomization test
+# inputs: 2 column data frame for regression
+# outputs: regression slope
+###################################
+get_metric <- function(z = NULL){
+  if(is.null(z)){
+    x_obs <- 1:20
+    y_obs <- x_obs + 10 * rnorm(20)
+    z <- data.frame(ID = seq_along(x_obs),
+                    x_obs,
+                    y_obs)
+  }
+  . <- lm(z[,3] ~ z[,2])
+  . <- summary(.)
+  . <- .$coefficients[2,1]
+  slope <- .
+return(slope)
 
+} # end of get_metric
+#--------------------------------
+#get_metric()
+# -------------------------------
+# FUNCTION shuffle_data
+# description: randomize data for regression analysis
+# inputs: 3 column data frame (ID, x, y)
+# outputs: 3 column data frame (ID, x, y)
+###################################
+shuffle_data <- function(z = NULL){
+  if(is.null(z)){
+    x_obs <- 1:20
+    y_obs <- x_obs + 10 * rnorm(20)
+    z <- data.frame(ID = seq_along(x_obs),
+                    x_obs,
+                    y_obs)
+  }
+z[,3] <- sample(z[,3])
+return(z)
 
+} # end of shuffle_data
+#--------------------------------
+# shuffle_data()
+# -------------------------------
+# FUNCTION get_pval
+# description: calculates p value from simulation
+# inputs: list of observed metric and vector of simulated metrics
+# outputs: lower and upper tail probability value
+###################################
+get_pval <- function(z = NULL){
+  if(is.null(z)){
+    z <- list(rnorm(1), rnorm(1000))
+  }
+    p_lower <- mean(z[[2]] <= z[[1]])
+    p_upper <- mean(z[[2]] >= z[[1]])
+  
 
+return(c(pL = p_lower, pU = p_upper))
 
+} # end of get_pval
+#--------------------------------
+# get_pval()
+# -------------------------------
+# FUNCTION plot_ran_test
+# description: create a ggplot of histogram of simulated values
+# inputs: list of observed metric and vector simulated metrics
+# outputs: saved ggplot graph
+###################################
+plot_ran_test <- function(z = NULL){
+  if(is.null(z)){
+    z <- list(rnorm(1), rnorm(1000))
+  }
+df <- data.frame(ID = seq_along(z[[2]]),
+                 sim_x = z[[2]])
+p1 <- ggplot(data = df, aes(x = sim_x))
+p1 + geom_histogram(aes(fill = I("coral"),
+                        colour = I("black"))) +
+  geom_vline(aes(xintercept = z[[1]], colour = "dodgerblue"))
 
+} # end of plot_ran_test
+#--------------------------------
+# plot_ran_test()
 
+# Put it all together ------------------------------
 
+n_sim <- 1000 # number of simulated datasets
+x_sim <- rep(NA, n_sim) # vector of NA values length of simulation
+df <- read_data() # using default values from our read_data function
+x_obs <- get_metric(df) # get slope of observed data
 
+for(i in seq_len(n_sim)){
+  x_sim[i] <- get_metric(shuffle_data(df))
+}
 
+slopes <- list(x_obs, x_sim)
 
+get_pval(slopes)
 
-
-
-
-
-
-
-
+plot_ran_test(slopes)
 
 
 
